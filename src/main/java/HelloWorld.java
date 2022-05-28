29f0690e99ec755ffd806a20d77ff5664b2a3d11
Abdelmoujib Megzari <Abdelmoujib.megzari@grenoble-inp.org>
2022-05-28 12:08:37 +0200

    easter egg

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+	String[] names={"skywalker","yoda","Obi-Wan","Kenobi", "Qui-Gon","Jinn"};
+        System.out.println("Hi, whats your name?");//are you a jedi?
+	Scanner input = new Scanner(System.in);
+        String myString = input.next();
+	for(String name :names){
+		if(myString.contains(name)){
+			System.out.println("May the Force be with you!");
+			return;
+		}
+	}
+	System.out.println("Hello " +myString);
+	
     }
 }
